bafd5a20c2083814b5d9bf83e572c9669ed7b538
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-02-06 19:14:25 -0700

    added missing headers

---
+++ sstcomp/include/SST.h
@@ -0,0 +1,48 @@
+//
+// _SST_h_
+//
+// Copyright (C) 2017-2025 Tactical Computing Laboratories, LLC
+// All Rights Reserved
+// contact@tactcomplabs.com
+//
+// See LICENSE in the top level directory for licensing details
+//
+
+// Header file to include all SST headers, so that Rev compiler warnings can be
+// turned off during third-party SST header inclusion.
+
+#ifndef _SST_H_
+#define _SST_H_
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#pragma GCC diagnostic ignored "-Wsuggest-override"
+#pragma GCC diagnostic ignored "-Wdouble-promotion"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wconversion"
+
+#if defined( __GNUC__ ) && !defined( __clang__ )
+#pragma GCC diagnostic ignored "-Wsuggest-final-methods"
+#pragma GCC diagnostic ignored "-Wsuggest-final-types"
+#endif
+
+// The #include order is important, so we prevent clang-format from reordering
+// clang-format off
+#include <sst/core/sst_config.h>
+#include <sst/core/component.h>
+#include <sst/core/event.h>
+#include <sst/core/interfaces/simpleNetwork.h>
+// #include <sst/core/interfaces/stdMem.h>
+#include <sst/core/link.h>
+#include <sst/core/output.h>
+#include <sst/core/statapi/stataccumulator.h>
+#include <sst/core/subcomponent.h>
+#include <sst/core/timeConverter.h>
+#include <sst/core/model/element_python.h>
+#include <sst/core/rng/mersenne.h>
+// clang-format on
+
+#pragma GCC diagnostic pop
+
+#endif
