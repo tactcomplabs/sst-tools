a592b86e17dc289fb7d809c6cbf3a78f06b47a9e
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-03-03 11:27:04 -0700

    initial subcomponent for checkpoint testing

---
+++ sstcomp/cptsubcomp/cptsubcomp.cc
@@ -0,0 +1,50 @@
+//
+// _cptsubcomp_cc_
+//
+// Copyright (C) 2017-2025 Tactical Computing Laboratories, LLC
+// All Rights Reserved
+// contact@tactcomplabs.com
+//
+// See LICENSE in the top level directory for licensing details
+//
+
+#include "cptsubcomp.h"
+#include "tcldbg.h"
+
+using namespace SST;
+using namespace SST::CPTSubComp;
+
+// cptSubCompVecInt
+cptSubCompVecInt::cptSubCompVecInt(ComponentId_t id, Params& params) {
+    max = params.find<size_t>("max", 1024);
+    seed = params.find<unsigned>("seed", 0);
+    assert(max>0);
+    rng = new SST::RNG::MersenneRNG(seed);
+    for (size_t i=0; i<max; i++)
+     tut.push_back(rng->generateNextInt32());
+}
+
+cptSubCompVecInt::~cptSubCompVecInt()
+{
+    if (rng) delete rng;
+}
+
+int cptSubCompVecInt::check()
+{
+    return 0;
+}
+
+void cptSubCompVecInt::update()
+{
+    
+}
+
+void cptSubCompVecInt::serialize_order(SST::Core::Serialization::serializer &ser)
+{
+    SST_SER(max);
+    SST_SER(seed);
+    SST_SER(tut);
+    SST_SER(rng);
+}
+
+// EOF
---
+++ sstcomp/cptsubcomp/cptsubcomp.h
@@ -0,0 +1,105 @@
+//
+// _cptsubcomp_h_
+//
+// Copyright (C) 2017-2025 Tactical Computing Laboratories, LLC
+// All Rights Reserved
+// contact@tactcomplabs.com
+//
+// See LICENSE in the top level directory for licensing details
+//
+
+#ifndef _SST_CPTSUBCOMP_H_
+#define _SST_CPTSUBCOMP_H_
+
+/*
+ * Modeled after src/sst/elements/simpleElementExample/basicSubComponent_subcomponent.h
+ * The intent is to provides subcomponents that handle a variety of complex datatypes
+ * in order to understand the nuances of generating checkpoints and their effects on
+ * checkpoint size.
+ * 
+ * cptSubCompAPI - inherits from SST::SubComponent. Defines the API for checkpoint subcomponents.
+ * cptVecInt - std:vector<int>  baseline scheme should invoke ser& once for entire vector
+ * cptVecPairIntInt - std::vector<std::pair<int,int>> - need to invoke ser& for .first and .second elements
+ */
+
+// clang-format off
+// -- Standard Headers
+// #include <map>
+// #include <vector>
+// #include <queue>
+// #include <random>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+
+// -- SST Headers
+#include "SST.h"
+#include "sst/core/subcomponent.h"
+// clang-format on
+
+namespace SST::CPTSubComp{
+
+// -------------------------------------------------------
+// CPTSubComp
+// -------------------------------------------------------
+
+class CPTSubCompAPI : public SST::SubComponent
+{
+public:
+  // Register subcomponent API
+  SST_ELI_REGISTER_SUBCOMPONENT_API(SST::CPTSubComp::CPTSubCompAPI)
+
+  // Constructor/Destructor
+  CPTSubCompAPI(ComponentId_t id, Params& params) : SubComponent(id) { }
+  virtual ~CPTSubCompAPI() {}
+
+  // API
+  // Subcomponent self-checking returns 0 if no errors
+  virtual int check() = 0;
+  // Update the subcomponent internal state
+  virtual void update() = 0;
+
+  // Serialization
+  CPTSubCompAPI();
+  ImplementVirtualSerializable(SST::CPTSubComp::CPTSubCompAPI);
+};
+
+// subcomponent implementation for std::vector<int>
+class cptSubCompVecInt : public CPTSubCompAPI {
+public:
+  SST_ELI_REGISTER_SUBCOMPONENT(
+    cptSubCompVecInt,     // Class name
+    "CPTSubComp",         // Library name, the 'lib' in SST's lib.name format
+    "cptSubCompVecInt",   // Name used to refer to this subcomponent, the 'name' in SST's lib.name format
+    SST_ELI_ELEMENT_VERSION(1,0,0), // A version number
+    "SubComponent for checkpoint type std::vector<int>", // Description
+    SST::CPTSubComp::CPTSubCompAPI // Fully qualified name of the API this subcomponent implements
+  )
+  SST_ELI_DOCUMENT_PARAMS( 
+    { "max", "Maximum number of test elements", "1024" },
+    { "seed","Initial seed for data generation", "0"}
+  )
+
+  cptSubCompVecInt(ComponentId_t id, Params& params);
+  ~cptSubCompVecInt();
+
+  // API members
+  int check() override;
+  void update() override;
+
+  // Serialization
+  void serialize_order(SST::Core::Serialization::serializer& ser) override;
+
+private:
+  size_t max;
+  unsigned seed;
+  std::vector<int32_t> tut;
+  SST::RNG::Random* rng;
+
+}; //class CPTSubCompAPI
+
+} // namespace SST::CPTSubComp
+
+#endif  // _SST_CPTSUBCOMP_H_
+
+// EOF
