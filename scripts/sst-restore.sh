09a8ca40f3dfd22785928b0bc3a3b945785cf5a7
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-03-07 16:11:27 -0700

    more scripting to avoid false positives on checkpoint tests

--- scripts/sst-restore.sh
+++ scripts/sst-restore.sh
@@ -1,20 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Synopsis
 #   ctest driver script for tests restoring checkpoints.
 # Usage
-#   sst-restore {cleanup} {mpi-ranks} {checkpoint-file} {rest of sst options}
+#   sst-restore {cleanup} {mpi-ranks} {cpt-prefix} {checkpoint-file} {rest of sst options}
 # Purpose
 #   This script provides mpirun prefix if needed
 
 cleanup=$1
 ranks=$2
-cpt=$3
-
-if [ -d ${cpt} ]; then
-    echo "WARNING: removing ${cpt}"
-    rm -rf ${cpt}
-fi
+pfx=$3
+cpt=$4
 
 if (( $ranks > 1)); then
     mpipfx="mpirun -n ${ranks}"
@@ -28,12 +24,12 @@ eval "${cmd}"
 if [ "$?" -eq 0 ]; then
     echo "Test passed"
     if [ "$cleanup" == "ON" ]; then
-        echo "Cleaning " ${cpt}:r
-        rm -rf ${cpt}:r
+        echo "Cleaning " ${pfx}
+        rm -rf ${pfx}
     fi
 else
     echo "Error: Test failed"
-    return 1
+    exit 1
 fi
 
 #EOF
--- test/cptapi/vecint-test.sh
+++ test/cptapi/vecint-test.sh
@@ -1,20 +1,75 @@
 #!/bin/bash
 
-rm -rf 2d_SAVE_VecInt*
+cleanup=$1
 
-echo "### saving checkpoint"
-sst --checkpoint-prefix=2d_SAVE_VecInt --num-threads=2 --add-lib-path=/Users/kgriesser/work/sst-tools/build/sstcomp/grid 2d.py --checkpoint-period=1us -- --x=2 --y=2 --subcomp=grid.CPTSubCompVecInt --verbose=2
+regex_list=()
+regex_list+=( 'GridNode[cp_0_1:finish:10000000]: cp_0_1 finish() clocks 10000 check 0xcc6800' )
+regex_list+=( 'CPTSubCompVecInt[cp_0_1:CPTSubComp:finish:10000000]: finish() clocks 10000 check 0xb97d34ed' )
+regex_list+=( 'GridNode[cp_1_1:finish:10000000]: cp_1_1 finish() clocks 10000 check 0xcc6800' )
+regex_list+=( 'CPTSubCompVecInt[cp_1_1:CPTSubComp:finish:10000000]: finish() clocks 10000 check 0x1f0aa967' )
+regex_list+=( 'GridNode[cp_0_0:finish:10000000]: cp_0_0 finish() clocks 10000 check 0xcc6800' )
+regex_list+=( 'CPTSubCompVecInt[cp_0_0:CPTSubComp:finish:10000000]: finish() clocks 10000 check 0x8771baba' )
+regex_list+=( 'GridNode[cp_1_0:finish:10000000]: cp_1_0 finish() clocks 10000 check 0xcc6800' )
+regex_list+=( 'CPTSubCompVecInt[cp_1_0:CPTSubComp:finish:10000000]: finish() clocks 10000 check 0xea51823d' )
+regex_list+=( 'Simulation is complete, simulated time: 10 us' )
 
+check() {
+    echo "### checking $1"
+    for regex in "${regex_list[@]}"; do
+        grep -qF "$regex" $1
+        if [ $? != 0 ]; then
+            echo "error: file $1 missing regex: ${regex}"
+            return 1
+        fi
+    done
+    return 0
+}
+
+pfx=2d_SAVE_VecInt
+logs=${pfx}_logs
+rm -rf $pfx $logs
+mkdir -p $logs
+
+gridlib=$(realpath ../../build/sstcmp/grid)
+
+echo "### creating checkpoints"
+sst --checkpoint-prefix=2d_SAVE_VecInt --num-threads=2 --checkpoint-period=1us \
+    --add-lib-path=${gridlib} \
+    2d.py -- --x=2 --y=2 --subcomp=grid.CPTSubCompVecInt --verbose=2 > ${logs}/save.log
 if [ $? != 0 ]; then
-    echo "checkpoint save failed"
+    echo "error: checkpoint save failed"
     exit 1
 fi
+check ${logs}/save.log
+if [ $? != 0 ]; then
+    echo "error: simulation check failed"
+    exit 10
+fi
 
-echo "### loading checkpoint"
-sst --load-checkpoint 2d_SAVE_VecInt/2d_SAVE_VecInt_4_5000000/2d_SAVE_VecInt_4_5000000.sstcpt --num-threads=2 --add-lib-path=/Users/kgriesser/work/sst-tools/build/sstcomp/grid
-
+for i in 0_1 1_2 2_3 3_4 4_5 5_6 6_7 7_8 8_9 9_10 10_11
+do
+    cpt=${pfx}_${i}000000
+    echo "### loading checkpoint ${cpt}"
+    sst --load-checkpoint ${pfx}/${cpt}/${cpt}.sstcpt \
+        --num-threads=2 \
+        --add-lib-path=${gridlib} > ${logs}/${i}.log
+    check ${logs}/${i}.log
+    if [ $? != 0 ]; then
+        echo "error: restore check failed for ${cpt}"
+        exit 11
+    fi
+done
 
 if [ $? != 0 ]; then
-    echo "checkpoint load failed"
+    echo "error: checkpoint load failed"
     exit 2
 fi
+
+# optional clean
+if [ "$cleanup" == "ON" ]; then
+    echo "Cleaning " ${logs} ${pfx}
+    rm -rf ${logs} ${pfx}
+fi
+
+# for ctest pass regexp
+echo "Simulation is complete"
