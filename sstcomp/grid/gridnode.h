16c9998cebcb299b0baf97a4bb5757a29de4cd1e
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-03-24 10:57:25 -0700

    missed a semicolon

--- sstcomp/grid/gridnode.h
+++ sstcomp/grid/gridnode.h
@@ -57,7 +57,7 @@ private:
   /// GridNodeEvent: serialization method
   void serialize_order(SST::Core::Serialization::serializer& ser) override{
     Event::serialize_order(ser);
-    SST_SER(data)
+    SST_SER(data);
   }
 
   /// GridNodeEvent: serialization implementor
