295952d8ed552f1369a94ca097ed7bbdf2fd54c3
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-09-06 08:55:44 -0700

    Python SDL clean up

--- test/neuralnet/test-image.py
+++ test/neuralnet/test-image.py
@@ -14,16 +14,16 @@ from enum import Enum
 
 parser = argparse.ArgumentParser(description="test image")
 
-parser.add_argument("--batchSize", type=int, help="number of images for each training batch", default=128)
-parser.add_argument("--classImageLimit", type=int, help="limited the number of images loaded per class", default=100000)
-parser.add_argument("--epochs", type=int, help="number of training rounds", default=1)
-parser.add_argument("--evalImage", type=str, help="path to a single evaluation image", default="")
-parser.add_argument("--evalImages", type=str, help="path to a collection of evaluation images", default="")
-parser.add_argument("--hiddenLayerSize", type=int, help="number of neurons in each hidden layer", default=128)
+parser.add_argument("--batchSize",            type=int,   help="number of images for each training batch", default=128)
+parser.add_argument("--classImageLimit",      type=int,   help="limited the number of images loaded per class", default=100000)
+parser.add_argument("--epochs",               type=int,   help="number of training rounds", default=1)
+parser.add_argument("--evalImage",            type=str,   help="path to a single evaluation image", default="")
+parser.add_argument("--evalImages",           type=str,   help="path to a collection of evaluation images", default="")
+parser.add_argument("--hiddenLayerSize",      type=int,   help="number of neurons in each hidden layer", default=128)
 parser.add_argument("--initialWeightScaling", type=float, help="scaling factor for random weights", default=0.1)
-parser.add_argument("--testImages", type=str, help="path to test data organized in class subdirectories", default="")
-parser.add_argument("--trainingImages", type=str, help="path to training data organized in class subdirectories", default="")
-parser.add_argument("--verbose", type=int, help="verbosity. 5=send/recv", default=1)
+parser.add_argument("--testImages",           type=str,   help="path to test data organized in class subdirectories", default="")
+parser.add_argument("--trainingImages",       type=str,   help="path to training data organized in class subdirectories", default="")
+parser.add_argument("--verbose",              type=int,   help="verbosity. 5=send/recv", default=1)  
 
 args = parser.parse_args()
 print("configuration:")
@@ -45,86 +45,84 @@ batch_controller.addParams({
 
 # Optimizer parameters
 optimizer_params = {
-  "verbose" : args.verbose,
   "learningRate" : 0.001,
   "decay" : 1e-3,
   "epsilon" : 1e-7,
   "beta_1" : 0.9,
-  "beta_2" : 0.999
- }
+  "beta_2" : 0.999,
+  "verbose" : args.verbose,
+}
 
-# Instantiate layers
-input   = sst.Component("input",   "neuralnet.NNLayer")
-input.setSubComponent(  "transfer_function", "neuralnet.NNInputLayer")
+class DenseLayer():
+  def __init__(self, name, inputs, neurons):
+    self.comp =  sst.Component(name,  "neuralnet.NNLayer")
+    self.transfer_function = self.comp.setSubComponent( 
+      "transfer_function", "neuralnet.NNDenseLayer" )
+    self.transfer_function.addParams({ 
+      "nInputs" : inputs, "nNeurons" : neurons,
+      "initialWeightScaling" : args.initialWeightScaling,
+      "verbose" : args.verbose })
+    self.optimizer = self.comp.setSubComponent(
+      "optimizer", "neuralnet.NNAdamOptimizer" )
+    self.optimizer.addParams({ "verbose" : args.verbose })
+    self.optimizer.addParams( optimizer_params )
 
-# Limit input images to 28 x 28.
-dense1  = sst.Component("dense1",  "neuralnet.NNLayer")
-subd1 = dense1.setSubComponent( "transfer_function", "neuralnet.NNDenseLayer")
-subd1.addParams( {"nInputs" : 28*28, "nNeurons" : args.hiddenLayerSize,
-                 "initialWeightScaling" : args.initialWeightScaling,
-                 "verbose" : args.verbose } )
-subd1Opt = dense1.setSubComponent( "optimizer", "neuralnet.NNAdamOptimizer")
-subd1Opt.addParams( optimizer_params )
+class ReLU():
+  def __init__(self, name):
+    self.comp = sst.Component(name,   "neuralnet.NNLayer")
+    self.comp.addParams( {"verbose" : args.verbose} )
+    self.transfer_function = self.comp.setSubComponent( 
+      "transfer_function", "neuralnet.NNActivationReLULayer")
+    self.transfer_function.addParams( { "verbose" : args.verbose })
 
-relu1   = sst.Component("relu1",   "neuralnet.NNLayer")
-relu1.setSubComponent(  "transfer_function", "neuralnet.NNActivationReLULayer")
-relu1.addParams( {"verbose" : args.verbose} )
+class Softmax():
+  def __init__(self, name):
+    self.comp = sst.Component(name, "neuralnet.NNLayer")
+    self.comp.addParams( {"verbose" : args.verbose} )
+    self.tranfer_function = self.comp.setSubComponent("transfer_function", "neuralnet.NNActivationSoftmaxLayer")
+    self.tranfer_function.addParams( {"verbose" : args.verbose} )
 
-dense2  = sst.Component("dense2",  "neuralnet.NNLayer")
-subd2 = dense2.setSubComponent( "transfer_function", "neuralnet.NNDenseLayer")
-subd2.addParams( {"nInputs" : args.hiddenLayerSize, "nNeurons" : args.hiddenLayerSize,
-                 "initialWeightScaling" : args.initialWeightScaling,
-                 "verbose" : args.verbose } )
-subd2Opt = dense2.setSubComponent( "optimizer", "neuralnet.NNAdamOptimizer")
-subd2Opt.addParams( optimizer_params )
+class Loss_CategoricalCrossEntropy:
+  def __init__(self, name):
+    self.comp = sst.Component(name,    "neuralnet.NNLayer")
+    self.comp.addParams({ 
+      "lastComponent" : 1,
+      "verbose" : args.verbose })
+    self.transfer_function = self.comp.setSubComponent( "transfer_function", "neuralnet.NNInputLayer")
+    self.transfer_function.addParams({ "verbose" : args.verbose })
+    self.accuracy_function = self.comp.setSubComponent( "accuracy_function", "neuralnet.NNAccuracyCategorical")
+    self.accuracy_function.addParams( {"verbose" : args.verbose} );
+    self.loss_function = self.comp.setSubComponent( "loss_function", "neuralnet.NNLoss_CategoricalCrossEntropy")
+    self.loss_function.addParams( {"verbose" : args.verbose} )
+    self.optimizer = self.comp.setSubComponent( "optimizer", "neuralnet.NNAdamOptimizer" )
+    self.optimizer.addParams( optimizer_params )
 
-relu2   = sst.Component("relu2",   "neuralnet.NNLayer")
-relu2.setSubComponent(  "transfer_function", "neuralnet.NNActivationReLULayer")
-relu2.addParams( {"verbose" : args.verbose} )
+# Layers
+image_size = 28 * 28
+input = sst.Component("input",   "neuralnet.NNLayer")
+input.setSubComponent("transfer_function", "neuralnet.NNInputLayer")
+dense1 = DenseLayer("dense1", image_size, args.hiddenLayerSize)
+relu1 = ReLU("relu1")
+dense2 = DenseLayer("dense2", args.hiddenLayerSize, args.hiddenLayerSize)
+relu2 = ReLU("relu2")
+dense3 = DenseLayer("dense3", args.hiddenLayerSize, 10)
+softmax = Softmax("softmax")
+loss = Loss_CategoricalCrossEntropy("loss")
 
-dense3  = sst.Component("dense3",  "neuralnet.NNLayer")
-subd3 = dense3.setSubComponent("transfer_function", "neuralnet.NNDenseLayer" )
-subd3.addParams( {"nInputs" : args.hiddenLayerSize, "nNeurons" : 10,
-                 "initialWeightScaling" : args.initialWeightScaling,
-                 "verbose" : args.verbose } )
-subd3Opt = dense3.setSubComponent( "optimizer", "neuralnet.NNAdamOptimizer")
-subd3Opt.addParams( optimizer_params )
-
-softmax = sst.Component("softmax", "neuralnet.NNLayer")
-subsoftmax = softmax.setSubComponent("transfer_function", "neuralnet.NNActivationSoftmaxLayer")
-subsoftmax.addParams( {"verbose" : args.verbose} )
-
-# Final stage: Loss Layer
-
-loss    = sst.Component("loss",    "neuralnet.NNLayer")
-loss.addParams( { "lastComponent" : 1, "verbose" : args.verbose} )
-
-loss_xfr = loss.setSubComponent( "transfer_function", "neuralnet.NNInputLayer")
-loss_xfr.addParams( {"verbose" : args.verbose} );
-
-loss_acc=loss.setSubComponent( "accuracy_function", "neuralnet.NNAccuracyCategorical")
-loss_acc.addParams( {"verbose" : args.verbose} );
-
-loss_fnc = loss.setSubComponent( "loss_function", "neuralnet.NNLoss_CategoricalCrossEntropy")
-loss_fnc.addParams( {"verbose" : args.verbose} );
-
-lossOpt = loss.setSubComponent( "optimizer", "neuralnet.NNAdamOptimizer" )
-lossOpt.addParams( optimizer_params )
-
-# Ordered lists
+# Connections
 components = []
 forward_links = []
 backward_links = []
 
 components.append(batch_controller)
 components.append(input)
-components.append(dense1)
-components.append(relu1)
-components.append(dense2)
-components.append(relu2)
-components.append(dense3)
-components.append(softmax)
-components.append(loss)
+components.append(dense1.comp)
+components.append(relu1.comp)
+components.append(dense2.comp)
+components.append(relu2.comp)
+components.append(dense3.comp)
+components.append(softmax.comp)
+components.append(loss.comp)
 
 for i in range(len(components)):
   components[i].addParams( { "verbose" : args.verbose } )
