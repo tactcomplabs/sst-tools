c43c1399c5d8185f33e79b5f31eff87142e7e604
Kenneth Griesser <kgriesser@tclcomplabs.com>
2026-01-14 15:52:28 -0700

    debug messages for clocks - do not commit

--- sstcomp/neuralnet/nn_layer.cc
+++ sstcomp/neuralnet/nn_layer.cc
@@ -44,8 +44,11 @@ NNLayer::NNLayer(SST::ComponentId_t id, const SST::Params& params ) :
   const std::string systemClock = params.find< std::string >("clockFreq", "1GHz");
   clockHandler_  = new SST::Clock::Handler2<NNLayer,&NNLayer::clockTick>(this);
   timeConverter_ = registerClock(systemClock, clockHandler_);
+  sstout_.verbose(CALL_INFO, 10, 0, "register clock: ref=%p factor=%" PRIx64 "\n", timeConverter_, timeConverter_->getFactor());
   // an event will wake up the clocking
+  sstout_.verbose(CALL_INFO, 10, 0, "unregister clock: ref=%p factor=%" PRIx64 "\n", timeConverter_, timeConverter_->getFactor());
   unregisterClock(timeConverter_, clockHandler_);
+ 
 
   // subcomponents
   if (lastComponent_) {
@@ -192,7 +195,9 @@ void NNLayer::forward_i_rcv(SST::Event *ev){
   }
   else
     driveForwardPass_ = true;
-
+ 
+  sstout_.verbose(CALL_INFO, 10, 0, "reregister clock: ref=%p factor=%" PRIx64 "\n", timeConverter_, timeConverter_->getFactor());
+  std::cout << "nn_layer.cc:201 ["  << sstout_.getPrefix() << "] reregister clock: ref=0x" << std::hex << timeConverter_ << " factor=0x" << timeConverter_->getFactor() << std::endl;
   reregisterClock(timeConverter_, clockHandler_);
   delete ev;
 }
@@ -201,7 +206,7 @@ void NNLayer::backward_i_rcv(SST::Event *ev){
   NNEvent *nnev = static_cast<NNEvent*>(ev);
   backwardData_i = nnev->payload();
   driveBackwardPass_ = true;
-
+  sstout_.verbose(CALL_INFO, 10, 0, "reregister clock: ref=%p factor=%" PRIx64 "\n", timeConverter_, timeConverter_->getFactor());
   reregisterClock(timeConverter_, clockHandler_);
   delete ev;
 }
